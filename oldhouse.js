2f99c9293081ae0e6c48f1fdd98d4ba9b43dcdf7
Brian Evasiuk <brianevasiuk@Brians-MacBook-Air-2.local>
2016-10-23 15:07:58 -0700

    bugs still present

--- oldhouse.js
+++ oldhouse.js
@@ -1,9 +1,15 @@
 var oldhouse = [];
+var imgurl=window.document.getElementById('portrait')
+var imgName=window.document.getElementById('portraitName')
+var imgSpeech=window.document.getElementById('portraitSpeech')
+var goodDeeds=0
 oldhouse[0] = function() {
-	var title = "A sunny hallway";
-	var description = "The sunlight streams in through the windows to the east, causing dust particles in the air to glimmer while drifting between the shafts of light.";
-	var exitsDescription = "The hall continues to the <strong>north</strong>. The great entry hall can be seen to the <strong>south</strong>.";
+	var title = "Log Cabin";
+	var description = "";
+	var exitsDescription = "";
 	var visited = false;
+	
+
 	this.actions = function(command, commandData) {
 		if (typeof (this[command]) == "function") {
 			return this[command](commandData);
@@ -23,6 +29,7 @@ oldhouse[0] = function() {
 		system.println("You walk north.");
 		system.println("");
 		player.setPosition(0, 1);
+
 		return true;
 	};
 	this.n = this.north;
@@ -101,9 +108,10 @@ oldhouse[1] = function() {
 };
 oldhouse[2] = function() {
 	var title = "Log Cabin";
-	var description = "You wake up";
-	var exitsDescription = "A hallway extends to the <strong>north</strong>. The door to the south does not look like it will open.";
+	var description = "<i>You are awoken by a sharp pain on your neck, noticing the deadliest spider of Massachusetts has bitten you. Because of your savvy outdoorsmanship, you come to the understanding you have around 24 hours left to live. You also regret passing up on the industrial insulation that would have kept spiders out of your home.  </i><br>";
+	var exitsDescription = "You consider <u>Stand</u>ing up from your <u>bed</u> to evaluate the situation. ";
 	var visited = false;
+
 	this.actions = function(command, commandData) {
 		if (typeof (this[command]) == "function") {
 			return this[command](commandData);
@@ -118,26 +126,52 @@ oldhouse[2] = function() {
 			system.println(exitsDescription);
 			visited = true;
 		}
-
 	};
-	this.north = function() {
-		system.println("You walk north.");
+	this.stand = function() {
+		system.println("...");
+		system.println("<i>You stand up and take a deep breath. The morning sunlight streams in through the curtainless windows, causing dust particles in the air to glimmer. Wally starts barking at you, hes probably hungry. On your table you see a pile of unattended IRS letters amounting to 2500$ in taxes due.</i>")
+		system.println("One of the tables legs is being supported by a bar of gold. Its worth approximitly a lifetime supply of dogfood, or 2500$. You consider heading down the <u>road</u> to the local village or going about what you planned on doing today anyways over at Walden <u>pond</u>. ");
 		system.println("");
-		player.setPosition(0, 0);
+		imgurl.innerHTML="<img src='img/dog.jpg'></img>"
+		imgName.textContent="Wally:"
+		imgSpeech.textContent="'Woof! Woof! (whimper)'"
 		return true;
 	};
-	this.n = this.north;
+	this.pond = function(){
+		system.title('Walden Pond')
+		system.println("...");
+		system.println("...");
+		imgurl.innerHTML="<img src='img/marcher.jpg'></img>"
+		imgName.textContent="John Marcher:"
+		imgSpeech.textContent="'...'"
+		return true
 
-	this.look = function(commandData) {
-		if (commandData.length <= 0) {
-			system.println("You look around.");
-			system.println("");
-			system.println(description);
-			system.println(exitsDescription);
+	};
+
+	this.bed = function(commandData) {
+		system.println("You sleep in for 24 hours");
+		system.println("...");
+		system.println("ENDING 1/6 (Quietism)");
+		imgurl.innerHTML="<img src='img/bartleby.jpg'></img>"
+		imgName.textContent="Ghost of Herman Melville:"
+		imgSpeech.textContent="'Ah Thoreau! Ah Humanity!'"
+		return true;
+	};
+	this.l = this.look;
+};
+oldhouse[4] = function() {
+	var title = "ENDING 1/6 (Are you seriously that lazy?)";
+	var description = "The polished marble floors echo with footsteps, augmenting the already voluminous size of the ballroom. In the center of the tremendous vaulted ceiling hangs an enormous crystal chandelier, casting sparkles of light across the ornately gilded walls.";
+	var exitsDescription = "A hallway extends beneath an archway to the <strong>south</strong>.";
+	var visited = false;
+	this.actions = function(command, commandData) {
+		if (typeof (this[command]) == "function") {
+			return this[command](commandData);
 		}
 		return false;
 	};
-	this.l = this.look;
+	
+
 };
 oldhouse[3] = function() {
 	var title = "Grand Ballroom";
